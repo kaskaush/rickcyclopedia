a333bc5d6ead7d70555d91393a6d05003958a750
kaushik kasibhatla <kasibhatla.kaushik@sapient.com>
2020-06-26 10:13:10 +0530

    Updates

---
+++ precache-manifest.4d4afe5caa3d0f5bbabfb57e4d7a264d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc915e45bd53e97d8614d9a5a75e0de6",
+    "url": "/rickcyclopedia/index.html"
+  },
+  {
+    "revision": "684ed32ebca429cd418a",
+    "url": "/rickcyclopedia/static/css/main.7418deac.chunk.css"
+  },
+  {
+    "revision": "5463b4b66334504250cd",
+    "url": "/rickcyclopedia/static/js/2.d931761e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rickcyclopedia/static/js/2.d931761e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684ed32ebca429cd418a",
+    "url": "/rickcyclopedia/static/js/main.491c281b.chunk.js"
+  },
+  {
+    "revision": "4dd9e804827e1076cdf7",
+    "url": "/rickcyclopedia/static/js/runtime-main.139cb0c3.js"
+  }
+]);
